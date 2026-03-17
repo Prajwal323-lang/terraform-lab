46a51bd26ea89d501fa34d9a90523ca2bae6182e
Prajwal323-lang <prajwal.agnihotri@zb.io>
2026-03-17 10:03:56 +0530

    Completed Day4: Terraform modules with multiple nginx containers

---
+++ day4/modules/nginx_container/variables.tf
@@ -0,0 +1,11 @@
+variable "container_name" {
+  description = "Name of the container"
+}
+
+variable "container_port" {
+  description = "Port exposed on host"
+}
+
+variable "image_name" {
+  description = "Docker image"
+}
